6a6fd755df07183f15a26a824874716e9ae0e3d4
Leah Oswald <mail@leahoswald.de>
2015-09-25 01:11:28 +0200

    increment version to v0.2.7

--- site.mk
+++ site.mk
@@ -36,7 +36,7 @@ GLUON_SITE_PACKAGES := \
 #			opkg compare-versions "$1" '>>' "$2"
 #		to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := 0.2+exp$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 0.2.7+exp$(shell date '+%Y%m%d')
 
 
 ##	GLUON_RELEASE
