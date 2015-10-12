95e706012a6b767d09c58b394c10743ac364d830
Leah Oswald <mail@leahoswald.de>
2015-10-12 22:50:46 +0200

    increase version to v0.4 first stable version

--- site.mk
+++ site.mk
@@ -37,7 +37,7 @@ GLUON_SITE_PACKAGES := \
 #			opkg compare-versions "$1" '>>' "$2"
 #		to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := 0.3.1+exp$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 0.4-$(shell date '+%Y%m%d')
 
 
 ##	GLUON_RELEASE
