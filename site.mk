0b5a666e7c7c2de1de698e1bd6a1fc5252486484
Ben Oswald <ben.oswald@root-space.de>
2016-11-24 16:05:55 +0100

    update to gluon 2016.2.1

--- site.mk
+++ site.mk
@@ -53,7 +53,7 @@ endif
 #			opkg compare-versions "$1" '>>' "$2"
 #		to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := 0.6.1-$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 0.6.2-$(shell date '+%Y%m%d')
 
 
 ##	GLUON_RELEASE
