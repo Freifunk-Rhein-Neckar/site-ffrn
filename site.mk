0c91aacdba4b9e2754fd172bfe3a66cf0e60fd22
Ben Oswald <ben.oswald@root-space.de>
2017-02-16 21:25:39 +0100

    add ffho feed to add filter for unwanted networks and bump to v0.6.5

--- site.mk
+++ site.mk
@@ -32,7 +32,7 @@ GLUON_SITE_PACKAGES := \
 	iptables \
 	iwinfo \
 	ffrn-lowmem-patches \
-#	ffrn-remove-old-vlan \
+	ffho-ebtables-net-rules
 
 ifeq ($(GLUON_TARGET),x86-generic)
 # support the usb stack on x86 devices
@@ -53,7 +53,7 @@ endif
 #			opkg compare-versions "$1" '>>' "$2"
 #		to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := 0.6.4-$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 0.6.5-$(shell date '+%Y%m%d')
 
 
 ##	GLUON_RELEASE
