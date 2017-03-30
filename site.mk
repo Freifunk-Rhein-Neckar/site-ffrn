38037744a3706db9341f889ae22d5f2daeaf1219
Leah Oswald <mail@leahoswald.de>
2017-03-30 17:18:34 +0200

    upgrade config to support gluon LEDE

--- site.mk
+++ site.mk
@@ -13,17 +13,16 @@ GLUON_SITE_PACKAGES := \
 	gluon-config-mode-core \
 	gluon-config-mode-geo-location \
 	gluon-config-mode-hostname \
-	gluon-luci-private-wifi \
 	gluon-config-mode-mesh-vpn \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
 	gluon-ebtables-segment-mld \
-	gluon-luci-admin \
-	gluon-luci-autoupdater \
-	gluon-luci-portconfig \
-	gluon-luci-wifi-config \
-	gluon-luci-node-role \
-	gluon-next-node \
+	gluon-web-admin \
+	gluon-web-autoupdater \
+	gluon-web-network \
+	gluon-web-wifi-config \
+	gluon-web-private-wifi \
+	gluon-web-node-role \
 	gluon-mesh-vpn-fastd \
 	gluon-radvd \
 	gluon-setup-mode \
@@ -35,18 +34,130 @@ GLUON_SITE_PACKAGES := \
 	ffho-ebtables-net-rules \
 	ffho-ath9k-blackout-workaround
 
-ifeq ($(GLUON_TARGET),x86-generic)
-# support the usb stack on x86 devices
-# and add a few common USB NICs
-GLUON_SITE_PACKAGES += \
-	kmod-usb-core \
-	kmod-usb2 \
+############################
+# Additional package sets
+#############################
+
+# USB Human Interface
+USB_PACKAGES_HID := \
 	kmod-usb-hid \
+	kmod-hid-generic
+
+# USB Serial
+USB_PACKAGES_SERIAL := \
+	kmod-usb-serial \
+	kmod-usb-serial-ftdi \
+	kmod-usb-serial-pl2303
+
+# USB Storage
+USB_PACKAGES_STORAGE := \
+	block-mount \
+	blkid \
+	kmod-fs-exfat \
+	kmod-fs-ext4 \
+	kmod-fs-ntfs \
+	kmod-fs-vfat \
+	kmod-usb-storage \
+	kmod-usb-storage-extras \
+	kmod-nls-base \
+	kmod-nls-cp1250 \
+	kmod-nls-cp1251 \
+	kmod-nls-cp437 \
+	kmod-nls-cp850 \
+	kmod-nls-cp852 \
+	kmod-nls-iso8859-1 \
+	kmod-nls-iso8859-13 \
+	kmod-nls-iso8859-15 \
+	kmod-nls-iso8859-2 \
+	kmod-nls-utf8 \
+	swap-utils
+
+# USB Wired Ethernet Network
+USB_PACKAGES_NET := \
+	kmod-mii \
 	kmod-usb-net \
 	kmod-usb-net-asix \
-	kmod-usb-net-dm9601-ether
+	kmod-usb-net-asix-ax88179 \
+	kmod-usb-net-cdc-eem \
+	kmod-usb-net-cdc-ether \
+	kmod-usb-net-cdc-subset \
+	kmod-usb-net-dm9601-ether \
+	kmod-usb-net-hso \
+	kmod-usb-net-mcs7830 \
+	kmod-usb-net-pegasus \
+	kmod-usb-net-rtl8152 \
+	kmod-usb-net-smsc95xx
+
+# PCI-Express Network
+PCIE_PACKAGES_NET := \
+	kmod-bnx2
+
+
+# Group previous package sets
+USB_PKGS_WITHOUT_HID := \
+	usbutils \
+	$(USB_PACKAGES_SERIAL) \
+	$(USB_PACKAGES_STORAGE) \
+	$(USB_PACKAGES_NET)
+
+USB_PKGS := \
+	$(USB_PACKAGES_HID) \
+	$(USB_FULL_WITHOUT_HID)
+
+PCI_PKSG := \
+	pciutils \
+	$(PCIE_PACKAGES_NET)
+
+
+##################################
+# Assign package sets to targets
+##################################
+
+# Embedded Routers
+ifeq ($(GLUON_TARGET),ar71xx-generic)
+GLUON_SITE_PACKAGES += $(USB_PKGS_WITHOUT_HID)
 endif
 
+ifeq ($(GLUON_TARGET),ar71xx-nand)
+GLUON_SITE_PACKAGES += $(USB_PKGS_WITHOUT_HID)
+endif
+
+ifeq ($(GLUON_TARGET),mpc85xx-generic)
+GLUON_SITE_PACKAGES += $(USB_PKGS_WITHOUT_HID)
+endif
+
+ifeq ($(GLUON_TARGET),ramips-mt7621)
+GLUON_SITE_PACKAGES += $(USB_PKGS_WITHOUT_HID)
+endif
+
+# x86 Generic Purpose Hardware
+ifeq ($(GLUON_TARGET),x86-generic)
+GLUON_SITE_PACKAGES += $(USB_PKGS) $(PCIE_PKGS)
+endif
+
+ifeq ($(GLUON_TARGET),x86-64)
+GLUON_SITE_PACKAGES += $(USB_PKGS) $(PCIE_PKGS)
+endif
+
+# PCEngines ALIX Boards
+ifeq ($(GLUON_TARGET),x86-geode)
+GLUON_SITE_PACKAGES += $(USB_PKGS) $(PCIE_PKGS)
+endif
+
+#  Raspberry Pi A/B/B+
+ifeq ($(GLUON_TARGET),brcm2708-bcm2708)
+GLUON_SITE_PACKAGES += $(USB_PKGS)
+endif
+
+# Raspberry Pi 2
+ifeq ($(GLUON_TARGET),brcm2708-bcm2709)
+GLUON_SITE_PACKAGES += $(USB_PKGS)
+endif
+
+# Banana Pi/Pro, Lamobo R1
+ifeq ($(GLUON_TARGET),sunxi)
+GLUON_SITE_PACKAGES += $(USB_PKGS)
+endif
 
 ##	DEFAULT_GLUON_RELEASE
 #		version string to use for images
@@ -54,7 +165,7 @@ endif
 #			opkg compare-versions "$1" '>>' "$2"
 #		to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := 0.6.7-$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 0.7.0-$(shell date '+%Y%m%d')
 
 
 ##	GLUON_RELEASE
@@ -68,7 +179,7 @@ DEFAULT_GLUON_RELEASE := 0.6.7-$(shell date '+%Y%m%d')
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 
 # Default priority for updates.
-GLUON_PRIORITY ?= 0
+GLUON_PRIORITY := 0
 
 # Languages to include
 GLUON_LANGS ?= de en
@@ -77,4 +188,3 @@ GLUON_LANGS ?= de en
 GLUON_REGION ?= eu
 
 GLUON_ATH10K_MESH ?= 11s
-
