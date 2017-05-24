e29b9a85af333dbb9eba9d2ad8ab4deafe2be794
Nurtic-Vibe <nurtic-vibe@grmml.eu>
2017-05-24 14:58:53 +0200

    Increase version to 0.7.1

--- site.mk
+++ site.mk
@@ -165,7 +165,7 @@ endif
 #			opkg compare-versions "$1" '>>' "$2"
 #		to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := 0.7.0-$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 0.7.1-$(shell date '+%Y%m%d')
 
 
 ##	GLUON_RELEASE
