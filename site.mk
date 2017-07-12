8e438f10f9139cc6a37e09afc5d80f3b34b6e1b2
Nurtic-Vibe <nurtic-vibe@grmml.eu>
2017-07-12 13:28:46 +0200

    Push version to 1.0.3

--- site.mk
+++ site.mk
@@ -165,7 +165,7 @@ endif
 #			opkg compare-versions "$1" '>>' "$2"
 #		to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := 1.0.2-$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 1.0.3-$(shell date '+%Y%m%d')
 
 
 ##	GLUON_RELEASE
