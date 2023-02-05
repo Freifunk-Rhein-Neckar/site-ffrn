0bef75fb03d1e3e6a3c237e1ff04e05b06a90e07
Tom Herbers <mail@tomherbers.de>
2023-02-05 23:12:39 +0100

    contrib/genpkglist.py: remove USB pkgs for tl-wr1043n-v5
    The packages aren't necessary because the v5 has no USB port.
    
    (cherry picked from commit a440c3585b253277b900eed7851ee997c4859e5e)

--- contrib/genpkglist.py
+++ contrib/genpkglist.py
@@ -213,8 +213,7 @@ targets.get('ath79-generic'). \
         'tp-link-archer-c7-v5',
         'tp-link-archer-c59-v1',
         'tp-link-tl-wr842n-v3',
-        'tp-link-tl-wr1043nd-v4',
-        'tp-link-tl-wr1043n-v5'
+        'tp-link-tl-wr1043nd-v4'
     ], pkglists=[PKGS_USB, PKGS_USB_NET, PKGS_USB_SERIAL, PKGS_USB_STORAGE])
 
 for target in ['ipq40xx-generic', 'ipq806x-generic', 'mpc85xx-p1010', 'mpc85xx-p1020', 'mvebu-cortexa9', 'rockchip-armv8', 'sunxi-cortexa7']:
